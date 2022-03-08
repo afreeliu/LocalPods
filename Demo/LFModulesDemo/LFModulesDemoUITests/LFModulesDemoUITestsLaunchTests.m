0f9caa7e21618de135028010a235bf3289f91ed7
afree_liu <afree_liu@163.com>
2022-03-08 10:11:13 +0800

    添加demo工程

---
+++ Demo/LFModulesDemo/LFModulesDemoUITests/LFModulesDemoUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  LFModulesDemoUITestsLaunchTests.m
+//  LFModulesDemoUITests
+//
+//  Created by lufee on 2022/3/8.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LFModulesDemoUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation LFModulesDemoUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
